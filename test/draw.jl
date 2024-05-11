92def5fc7c4f5d84ec0dca7fce060f734329aaec
Tim Holy <tim.holy@gmail.com>
2024-05-11 04:44:35 -0500

    Bump [compat] on MakieCore
    This also adds "smoke tests" for the drawing functions. They do not run
    as part of CI, but seem potentially useful.

---
+++ test/draw.jl
@@ -0,0 +1,24 @@
+# Smoke tests for drawing functions
+
+using Makie   # load GLMakie or CairoMakie externally
+using GaussianMixtureAlignment
+using Test
+
+@testset "drawing" begin
+    tetrahedral = [
+        [0.,0.,1.],
+        [sqrt(8/9), 0., -1/3],
+        [-sqrt(2/9),sqrt(2/3),-1/3],
+        [-sqrt(2/9),-sqrt(2/3),-1/3]
+    ]
+    gmm = IsotropicGMM([IsotropicGaussian(x, 1.2, 1) for x in tetrahedral])
+    gmmdisplay(gmm)
+
+    ch_g = IsotropicGaussian(tetrahedral[1], 1.0, 1.0)
+    s_gs = [IsotropicGaussian(x, 0.5, 1.0) for (i,x) in enumerate(tetrahedral)]
+    mgmmx = IsotropicMultiGMM(Dict(
+        :positive => IsotropicGMM([ch_g]),
+        :steric => IsotropicGMM(s_gs)
+    ))
+    gmmdisplay(gmm)
+end
